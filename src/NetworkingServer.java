191483390f2121d55cb59700209dff799ec46f19
yusuf.sahel <yusuf.sahel@elev.ga.ntig.se>
2024-05-26 21:26:02 +0200

    nätverkslabb

---
+++ src/NetworkingServer.java
@@ -0,0 +1,50 @@
+
+/* SERVER – may enhance to work for multiple clients */
+import java.net.*;
+import java.io.*;
+
+public class NetworkingServer {
+
+    public static void main(String [] args) {
+
+        ServerSocket server = null;
+        Socket client;
+
+        // Vi använder standard portnummer.
+        int portnumber = 1234;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        //Skapa uttag på server sidan
+        try {
+            server = new ServerSocket(portnumber);
+        } catch (IOException ie) {
+            System.out.println("Cannot open socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " + server);
+
+        // Vänta på data från klient och svara
+        while(true) {
+
+            try {
+
+                // Lyssna efter en anslutning som ska göras
+                // detta uttag och accepterar det. Metoden blockerar tills
+                //en anslutning görs.
+                // a connection is made
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request is accepted...");
+                String clientHost = client.getInetAddress().getHostAddress();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
+
+                // Läs data från klient
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new
+                        InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = " + msgFromClient);
